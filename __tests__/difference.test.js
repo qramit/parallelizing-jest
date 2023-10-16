fa843512e103022b840272cf5d9bc618efecbde5
amit.shrivastava <amit.shrivastava@quantumrhino.com>
2023-10-16 17:11:54 +0200

    test if it detects test files

--- __tests__/difference.test.js
+++ __tests__/difference.test.js
@@ -1,4 +1,4 @@
-const difference = require("./src/difference");
+const difference = require("../src/difference");
 
 test("subtracts 1 - 2 to equal -1", () => {
   expect(difference(1)(2)).toBe(-0);
