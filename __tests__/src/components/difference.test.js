699cbbf241d6084b39848ba602a86443a2cbe0e6
amit.shrivastava <amit.shrivastava@quantumrhino.com>
2023-10-16 18:44:03 +0200

    Try test

--- __tests__/src/components/difference.test.js
+++ __tests__/src/components/difference.test.js
@@ -1,4 +1,4 @@
-const difference = require("../src/difference");
+const difference = require("../../../src/components/difference");
 
 test("subtracts 1 - 2 to equal -1", () => {
   expect(difference(1)(2)).toBe(-1);
--- __tests__/src/components/quotient.test.js
+++ __tests__/src/components/quotient.test.js
@@ -1,4 +1,4 @@
-const quotient = require("../src/quotient");
+const quotient = require("../../../src/components/quotient");
 
 test("divides 1 / 2 to equal .5", () => {
   expect(quotient(1)(2)).toBe(0.5);
